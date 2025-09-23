71cfe0afb0b160ce52dc2f8b86fb400de156e7fd
Vahagn Vardanyan <vahagn.vardanyan2@picsart.com>
2025-09-23 20:54:31 +0400

    replaced home page to crop page

--- src/constants/navigation.ts
+++ src/constants/navigation.ts
@@ -1,9 +1,6 @@
 import { HeaderNavigationItem } from '@/types/navigation';
 
 export const navigationItems: Array<HeaderNavigationItem> = [
-  { title: 'Home', href: '/' },
-  { title: 'Crop Image', href: '/crop-image' },
+  { title: 'Crop', href: '/' },
   { title: 'Resize Image', href: '/resize-image' },
-  { title: 'Services', href: '/services' },
-  { title: 'Upload', href: '/contact' },
 ];
