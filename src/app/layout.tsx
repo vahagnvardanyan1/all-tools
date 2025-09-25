2dfa0a7815cef60636b702f98b38ef4849368a54
Vahagn Vardanyan <vahagn.vardanyan2@picsart.com>
2025-09-26 01:13:53 +0400

    SpeedInsights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css';
 import type { Metadata } from 'next';
 import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 import AppRouterCacheProviderWrapper from '@/providers/AppRouterCacheProviderWrapper';
 import Header from '@/components/Header';
